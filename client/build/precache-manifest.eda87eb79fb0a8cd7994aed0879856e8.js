f64257d6ab4d8379e4e924e15703775aad043e76
Jason Brunelle <jason@jasonbrunelle.com>
2020-02-10 17:31:43 -0600

    Add client\build back to git

---
+++ client/build/precache-manifest.eda87eb79fb0a8cd7994aed0879856e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c35e30cd801af7ccee4f5a457b53aca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38adb6832628dd1ae6fa",
+    "url": "/static/css/main.63ea1612.chunk.css"
+  },
+  {
+    "revision": "2a3cdc40f9cc4b337378",
+    "url": "/static/js/2.b4b2ceef.chunk.js"
+  },
+  {
+    "revision": "38adb6832628dd1ae6fa",
+    "url": "/static/js/main.fe6fc733.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  }
+]);
